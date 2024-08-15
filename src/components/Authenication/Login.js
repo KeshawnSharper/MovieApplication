78d2fa396174f8d0f58be0625639e7635e8036eb
KESHAWN SHARPER <ksharper@studentmba.org>
2024-08-15 14:47:25 -0400

    hcfj

--- src/components/Authenication/Login.js
+++ src/components/Authenication/Login.js
@@ -29,7 +29,7 @@ function Login (props){
     setLoading(true)
     console.log(user)
     axios
-      .post(`https://movie-app-be-bsn.vercel.app/ login`, user)
+      .post(`https://movie-app-be-bsn.vercel.app/login`, user)
       .then((res) => {
         setLoading(false)
         console.log(res,"res")
