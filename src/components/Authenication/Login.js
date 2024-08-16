e6c9f457516a227d573c8fdd3a0c64686accd2ea
KESHAWN SHARPER <ksharper@studentmba.org>
2024-08-16 08:27:33 -0400

    cors7

--- src/components/Authenication/Login.js
+++ src/components/Authenication/Login.js
@@ -29,7 +29,7 @@ function Login (props){
     setLoading(true)
     console.log(user)
     axios
-      .post(`https://movie-app-be-bsn.vercel.app/login`, user)
+      .post(`https://movie-app-be-1.onrender.com/login`, user)
       .then((res) => {
         setLoading(false)
         console.log(res,"res")
